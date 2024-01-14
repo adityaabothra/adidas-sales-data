559835df998c58e298d253b28236e4b5658c7586
Aditya Bothra <31595609+adityaabothra@users.noreply.github.com>
2024-01-14 11:53:37 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -53,7 +53,7 @@ selected_retailers = st.multiselect("Select Retailers", df["Retailer"].unique(),
 filtered_df = df[df["Retailer"].isin(selected_retailers)].sort_values(by="Total Sales", ascending=False)
 
 col1, col2 = st.columns(2,gap="medium")
-
+st.subheader('Categorigal Distribution and Market Share')
 # Sunburst Chart
 with col1:
     st.subheader('Retailer Wise Categorical Sunburst Chart')
@@ -74,13 +74,6 @@ with col2:
     fig = px.pie(market_share, values=market_share, names=market_share.index, title='Market Share of Retailers')
     # Show the plot
     st.plotly_chart(fig)
-
-# Sunburst Chart
-st.subheader('Retailer Wise Categorical Sunburst Chart')
-sunburst_chart = px.sunburst(filtered_df, path=['Retailer', 'Product'], values='Total Sales',
-                             title='Sunburst Chart')
-st.plotly_chart(sunburst_chart, use_container_width=True)
-
 
 st.subheader("Bar Race Chart - Total Sales by Retailer")
 
@@ -115,40 +108,6 @@ if st.button("Start Race"):
         )
         bar_race_chart.plotly_chart(updated_race)    
         
-
-st.subheader("Market Share")
-# Group the data by retailer and sum the total sales for each retailer
-retailer_sales = filtered_df.groupby('Retailer')['Total Sales'].sum()
-# Calculate the total sales of all retailers
-total_sales = retailer_sales.sum()
-# Calculate the market share of each retailer by dividing their total sales by the total sales of all retailers
-market_share = retailer_sales / total_sales
-# Create a pie chart using plotly
-fig = px.pie(market_share, values=market_share, names=market_share.index, title='Market Share of Retailers')
-# Show the plot
-st.plotly_chart(fig)
-
-# st.subheader('Cumulative Retailer Sales Trendline Animation')
-# animated_trendline_place = st.empty()
-
-# # Animated trendline chart for cumulative sales comparison
-# animated_trendline_cumulative = px.scatter(
-#     df,
-#     x='Total Sales',
-#     y='Cumulative Total Sales',
-#     color='Retailer',
-#     title='Cumulative Retailer Sales Trendline',
-#     labels={'Cumulative Total Sales': 'Cumulative Sales'},
-#     animation_frame='Total Sales',
-#     animation_group='Retailer',
-#     width=800,
-#     height=500,
-#     trendline='ols'  # Ordinary Least Squares trendline
-# )
-
-# # Display the animated trendline chart
-# if st.button("Start"):
-#     animated_trendline_place.plotly_chart(animated_trendline_cumulative)
 
 yearly_sales = df.groupby(['Year','Month'])['Total Sales'].sum().reset_index()
 yearly_profit = df.groupby(['Year','Month'])['Operating Profit'].sum().reset_index()
